b216b265871011b667d0f8e3ea277fbeb202e780
dave griffiths <dave@fo.am>
2018-02-20 13:55:52 +0000

    removed C++ warnings

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -5,38 +5,19 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := starwisp-core
 
-LOCAL_CFLAGS := -DANDROID_NDK -fpermissive -DDISABLE_IMPORTGL -O3 -Wno-write-strings -DNDEBUG
+LOCAL_CFLAGS := -DANDROID_NDK -Wswitch -fpermissive -DDISABLE_IMPORTGL -O3 -Wno-write-strings -DNDEBUG
 APP_OPTIM := release
 
 LOCAL_SRC_FILES := \
-	fluxa/Sample.cpp \
-	fluxa/Allocator.cpp \
-	fluxa/Graph.cpp \
-	fluxa/GraphNode.cpp \
-	fluxa/Modules.cpp \
-	fluxa/ModuleNodes.cpp \
 	core/list.cpp \
 	core/db.cpp \
 	core/db_container.cpp \
 	core/fixed.cpp \
-	core/geometry.cpp \
 	core/idmap.cpp \
-	core/noise.cpp \
 	sqlite/sqlite3.c \
 	scheme/scheme.cpp \
-	engine/primitive.cpp \
-	engine/text_primitive.cpp \
-	engine/scenenode.cpp \
-	engine/scenegraph.cpp \
-	engine/texture.cpp \
-    engine/importgl.c \
-	engine/obj_reader.cpp \
-    engine/nomadic.cpp \
-	engine/engine.cpp \
-	jellyfish/jellyfish.cpp \
-	jellyfish/jellyfish_primitive.cpp \
-	audio.cpp \
-    app-android.c
+	app.cpp \
+	app-android.c
 
 LOCAL_LDLIBS := -lGLESv1_CM -lOpenSLES -ldl -llog
 
--- android/app/src/main/jni/app.h
+++ android/app/src/main/jni/app.h
@@ -4,16 +4,9 @@
 extern "C" {
 #endif
 
-#define WINDOW_DEFAULT_WIDTH    640
-#define WINDOW_DEFAULT_HEIGHT   480
-#define WINDOW_BPP              16
-
 extern void appInit();
-extern void initGL();
 extern void appDeinit();
-extern void appEval(char *code);
-extern void appRender(int width, int height);
-extern unsigned int appLoadTexture(const char *filename, int width, int height, char* data);
+extern void appEval(const char *code);
 
 #ifdef __cplusplus
 }
